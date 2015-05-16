d1f62db5f95e181eb07b3d789f5bfd4cfc80719a
Berkant AYDIN <ba@berkantaydin.com.tr>
2015-05-16 17:33:31 +0300

    16 May 2015 Cts EEST 17:33:29

--- publishconf.py
+++ publishconf.py
@@ -16,6 +16,7 @@ RELATIVE_URLS = False
 #FEED_ALL_ATOM = 'feeds/all.atom.xml'
 #CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 USE_PAGER = True
+ADDTHIS_PROFILE = 'ra-4f71fbfb48f55785'
 
 DELETE_OUTPUT_DIRECTORY = True
 
